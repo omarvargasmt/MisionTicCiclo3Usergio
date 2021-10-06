29606461c7fe0d1651f0b0747a13914b66afacbd
OMAR VARGAS <omar.vargas.mt@correo.usa.edu.co>
2021-10-06 16:45:09 -0500

    rename files

--- js/Ejemplos/funcionesrequest.js
+++ js/Ejemplos/funcionesrequest.js
@@ -1,116 +1,44 @@
-//Funcion para dibujar la tabla
-function pintarRespuesta(items) {  
-    
-    //Definir encabezados de la tabla
-    var tabla = `<table border="1"> 
-                <tr> 
-                <th colspan="2">ACCIONES</th> 
-                <th>ID</th> 
-                <th>NOMBRE</th> 
-                <th>MARCA</th> 
-                <th>MODELO</th> 
-                <th>ID_CATEGORIA</th> </tr>`;
-    
-    //Recorrer los elementos del arreglo y retornarlos en la tabla
-    for (var i=0; i < items.length; i++) {
-        tabla += `<tr>
-                    <th><button onclick='editarElemento(${items[i].id})'>Editar</button></th>
-                    <th><button onclick='borrarInformacion(${items[i].id})'>Borrar</button></th>
-                    <th>${items[i].id}</th>
-                    <th>${items[i].name}</th>
-                    <th>${items[i].brand}</th>
-                    <th>${items[i].model}</th>
-                    <th>${items[i].category_id}</th>
-                    </tr>`;
-    }
-    tabla += '</table>';
+//Funcion para llamados ajax usando objetos XMLHttpRequest
+function loadDoc() { 
+    //1. defino el objeto XMLHttpRequest
+    const xhttp = new XMLHttpRequest();
 
-    $("#listado").append(tabla);
-}
-
-//Funcion para consulta usando Ajax y JQuery
-function mostrarInformacion() {  
-    $.ajax({
-
-        url : 'https://gf255a7993b1618-bdclientes.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/clientes/clientes', 
-        type : 'GET', 
-        dataType : 'json', 
-
-        // código a ejecutar si la petición es satisfactoria;
-        success : function(respuesta) {
-            console.log(respuesta); //Esta instrucción permite ver en la consola del navegador la información contenida en respuesta.
-            pintarRespuesta(respuesta.items);
-        },
-
-        // código a ejecutar si la petición falla;
-        error : function(xhr, status) {
-            alert('Ha sucedido un problema');
-        },
+    //4. Cuando el servidor responde la petición, se ejecuta el evento y la funcion
+    xhttp.onload = function () {  
         
-        // código a ejecutar sin importar si la petición falla o no
-        complete : function(xhr, status) {
-            alert('Ejecutando petición');
+        //Longitud del arreglo
+        const datos = JSON.parse(this.responseText);
+        const longitud = datos.items.length;
+
+        //Definir el encabezado de la tabla
+        var tabla = "<table border='1'" +
+            "<tr>"+
+            "<th>Identificación</th>" +
+            "<th>Nombre</th>" +
+            "<th>Marca</th>" +
+            "<th>Modelo</th>" +
+            "<th>ID_Categoria</th>" +
+            "</tr>"
+
+        //Recorrer los elementos del arreglo y retornarlos en la tabla
+        for (var i =0; i < longitud; i++) {
+            tabla = tabla + "<tr>"+
+            "<th>" + datos.items[i].id + "</th>" +
+            "<th>" + datos.items[i].name + "</th>" +
+            "<th>" + datos.items[i].brand + "</th>" +
+            "<th>" + datos.items[i].model + "</th>" +
+            "<th>" + datos.items[i].category_id + "</th>" +
+            "</tr>";
         }
-    });
-}
 
-//Funcion para guardar información usando Ajax y JQuery
-function guardarInformacion() {  
-    var myData={
-        id:$("#id").val(),
-        name:$("#name").val(),
-        brand:$("#brand").val(),
-        model:$("#model").val(),
-        category_id:$("#category_id").val(),
-    };
+        tabla = tabla + "</table>"
 
-    var dataToSend=JSON.stringify(myData);
+        console.log(tabla);
 
-    $.ajax({
-        url : 'https://gf255a7993b1618-bdclientes.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/clientes/clientes', 
-        type : 'POST', 
-        data: myData,
-        dataType : 'JSON', 
-
-        success : function(respuesta) {
-            $("resultado").empty();
-            $("#id").val("");
-            $("#name").val("");
-            $("#brand").val("");
-            $("#model").val("");
-            $("#category_id").val("");
-            mostrarInformacion();
-            alert("Información guardada")
-        },
-
-        error : function(xhr, status) {
-            alert('Ha sucedido un problema');
-        }
-    });
-}
-
-function borrarInformacion(idRegistro) {  
-    var myData={
-        id:idRegistro
-    };
-
-    var dataToSend=JSON.stringify(myData);
-
-    $.ajax({
-        url : 'https://gf255a7993b1618-bdclientes.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/clientes/clientes', 
-        type : 'DELETE', 
-        data: myData,
-        dataType : 'JSON', 
-        contentType: "application/json",
-
-        success : function(respuesta) {
-            $("resultado").empty();
-            mostrarInformacion();
-            alert("Registro Eliminado")
-        },
-
-        error : function(xhr, status) {
-            alert('Ha sucedido un problema');
-        }
-    });
+        document.getElementById("demo").innerHTML = tabla;
+    }
+    //2. Definir la petición
+    xhttp.open("GET","https://gf255a7993b1618-bdclientes.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/clientes/clientes")
+    //3. Enviar la petición
+    xhttp.send();
 }
