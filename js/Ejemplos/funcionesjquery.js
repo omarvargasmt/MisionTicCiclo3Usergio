49e6cd3e4a7408c5eeae85c2a9b170d711e01c4b
OMAR VARGAS <omar.vargas.mt@correo.usa.edu.co>
2021-10-06 17:16:04 -0500

    update funciones jquery

--- js/Ejemplos/funcionesjquery.js
+++ js/Ejemplos/funcionesjquery.js
@@ -37,19 +37,19 @@ function mostrarInformacion() {
         dataType : 'json', 
 
         // código a ejecutar si la petición es satisfactoria;
-        success : function(respuesta) {
-            console.log(respuesta); //Esta instrucción permite ver en la consola del navegador la información contenida en respuesta.
+        success: function(respuesta){
             pintarRespuesta(respuesta.items);
+            console.log(respuesta.items);
         },
 
         // código a ejecutar si la petición falla;
-        error : function(xhr, status) {
+        error: function (xhr, status) {
             alert('Ha sucedido un problema');
         },
         
         // código a ejecutar sin importar si la petición falla o no
-        complete : function(xhr, status) {
-            alert('Ejecutando petición');
+        complete: function (xhr, status) {
+            alert('Ejecutando peticion');
         }
     });
 }
