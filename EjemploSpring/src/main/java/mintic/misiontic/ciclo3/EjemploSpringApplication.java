92075b317cd49c7436f9ce8234496c9b44084aee
OMAR VARGAS <omar.vargas.mt@correo.usa.edu.co>
2021-10-06 13:15:09 -0500

    project front end disfraces

---
+++ EjemploSpring/src/main/java/mintic/misiontic/ciclo3/EjemploSpringApplication.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploSpringApplication.class, args);
+	}
+
+}
---
+++ EjemploSpring/src/test/java/mintic/misiontic/ciclo3/EjemploSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
