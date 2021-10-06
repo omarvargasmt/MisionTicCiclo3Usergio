eb2d8b676d9ff8c937e56f79c9003638ce2b5e1f
OMAR VARGAS <omar.vargas.mt@correo.usa.edu.co>
2021-10-06 13:53:00 -0500

    update listar

--- js/listar.js
+++ js/listar.js
@@ -65,24 +65,24 @@ function listarRespuesta(items) {
     //define variable javascript con la definicion inicial de la tabla, la primera fila y los
     //encabezados o títulos de la tabla
     var tabla = `<table border="1">
-                  <tr>
+                <tr>
                     <th>Marca</th>
                     <th>Modelo</th>
                     <th>Categoria</th>
                     <th>Nombre</th>
                     <th colspan="2">Acciones</th>
-                  </tr>`;
-                  
+                </tr>`;
+                
     //recorre el arreglo de 'items' y construye dinamicamente la fila de datos de la tabla
     for (var i=0; i < items.length; i++) {
         tabla +=`<tr>
-                   <td>${items[i].brand}</td>
-                   <td>${items[i].model}</td>
-                   <td>${items[i].category_id}</td>
-                   <td>${items[i].name}</td>
-                   <td><button onclick="editarRegistro(${items[i].id})">Editar</button></td>
-                   <td><button onclick="borrarRegistro(${items[i].id})">Borrar</button></td>
-                   </tr>`;
+                    <td>${items[i].brand}</td>
+                    <td>${items[i].model}</td>
+                    <td>${items[i].category_id}</td>
+                    <td>${items[i].name}</td>
+                    <td><button onclick="editarRegistro(${items[i].id})">Editar</button></td>
+                    <td><button onclick="borrarRegistro(${items[i].id})">Borrar</button></td>
+                    </tr>`;
     }
 
     //cierra tabla agregando el tag adecuado
